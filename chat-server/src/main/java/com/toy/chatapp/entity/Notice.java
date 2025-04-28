717f358976a49db739a3bd87cfcdbbba94ddef60
mylog6190 <adg6190@naver.com>
2025-04-29 00:00:29 +0900

    add board entities

---
+++ chat-server/src/main/java/com/toy/chatapp/entity/Notice.java
@@ -0,0 +1,20 @@
+package com.toy.chatapp.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Notice {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private String content;
+    private String write;
+    private LocalDateTime createAt;
+}
